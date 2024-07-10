8e3d8f2c3c6bdcc8aedbbbf6bf1babd677af3927
sanjeev kumar <sanjeev.kumar@insightgeeks.in>
2024-07-10 10:31:32 +0000

    implement InMemoryUserDetailsManager concept

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 master branch always new springBoot project 
 springSecuritySection1= spring default spring security provide
-springsecsection2= custom url permission
-
+springsecsection2 = custom url permission
+springsecsection3 = implement InMemoryUserDetailsManager concept. 
--- src/main/java/com/bvr/LearningProject/config/ProjectSecurityConfig.java
+++ src/main/java/com/bvr/LearningProject/config/ProjectSecurityConfig.java
@@ -4,39 +4,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     /**
-     *  Below is the custom security configurations
+     * Below is the custom security configurations
      */
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-/*
-        httpSecurity.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+        httpSecurity.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-*/
 
-
-        /**
-         *  Configuration to deny all the requests
-         */
-        /*httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-*/
-        /**
-         *  Configuration to permit all the requests
-         */
-        httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
